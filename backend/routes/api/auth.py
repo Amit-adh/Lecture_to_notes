4f6ea1b6f81e2b2bff88bd646f5366066a7bf0d8
unknown <itsman38@gmail.com>
2025-06-05 11:10:42 +0545

    init backend portion

---
+++ backend/routes/api/auth.py
@@ -0,0 +1,8 @@
+from flask import Blueprint, request
+
+auth = Blueprint("auth", __name__)
+
+@auth.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        pass
